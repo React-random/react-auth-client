261a0b3bf9639814a54b54d06ba6c5ee32109027
jandie <eidnaj@live.nl>
2018-06-15 12:41:05 +0200

    Header component added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {Router} from 'react-router-dom';
 
 import App from './components/App';
 
 ReactDOM.render(
-    <App/>,
+    <App/>
+    ,
     document.querySelector('#root')
 );
