5cf712e5a8b11b7607f8c490660ce8ddac1641d8
jandie <eidnaj@live.nl>
2018-06-15 14:14:16 +0200

    Sign in implemented

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,22 @@
-import { AUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR } from './types';
 import axios from 'axios';
 
 const API_URL = 'http://localhost:3090';
 
-export const signup = (credentials) => async dispatch => {
-    const response = await axios.post(`${API_URL}/signup`, credentials);
+export const signup = (credentials, callback) => async dispatch => {
+    try{
+        const response = await axios.post(`${API_URL}/signup`, credentials);
 
-    dispatch({
-        types: AUTH_USER,
-        payload: response.data.token
-    });
+        dispatch({
+            type: AUTH_USER,
+            payload: response.data.token
+        });
+
+        callback();
+    } catch (e) {
+        dispatch({
+            type: AUTH_ERROR,
+            payload: 'Email in use'
+        });
+    }
 };
